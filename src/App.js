5d19f9960e79e0bdd73477c3c46a2dea367887d2
Pascal Vaccaro <pascal.vaccaro@gmail.com>
2023-02-10 13:03:40 +0100

    le titre “Google” en plein milieu de la page, en 4 couleurs

--- src/App.js
+++ src/App.js
@@ -23,7 +23,14 @@ function App() {
       </header>
 
       <main className="App-main">
-        <h1>Google</h1>
+        <h1>
+          <span className="blue">G</span>
+          <span className="red">o</span>
+          <span className="yellow">o</span>
+          <span className="blue">g</span>
+          <span className="green">l</span>
+          <span className="red">e</span>
+        </h1>
         <input type="text" />
         <section className="App-actions">
           <button>Recherche Google</button>
