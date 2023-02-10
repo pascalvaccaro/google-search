8a67635987e4df85bb4bc5aec41cc52daef4e87c
Pascal Vaccaro <pascal.vaccaro@gmail.com>
2023-02-10 13:33:14 +0100

    isole le composant Footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import Header from './components/Header';
 import Search from './components/Search';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Search />     
-
-      <footer className="App-footer">
-        France
-      </footer>
+      <Search />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="App-footer">
+      France
+    </footer>
+  );
+}
+
+export default Footer;
