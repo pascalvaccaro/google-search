68b75bf5c7b5a71564a5ac851a087544c981cbf3
Pascal Vaccaro <pascal.vaccaro@gmail.com>
2023-02-10 13:31:26 +0100

    isole le composant Header

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import loupe from './loupe.svg';
 import micro from './micro.svg';
 import camera from './camera.js';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://mail.google.com/mail/&ogbl"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Gmail
-        </a>
-        <a
-          className="App-link"
-          href="https://www.google.fr/imghp?hl=fr&ogbl"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Images
-        </a>
-      </header>
+      <Header />
 
       <main className="App-main">
         <h1>
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+function Header() {
+  return (
+    <header className="App-header">
+      <a
+        className="App-link"
+        href="https://mail.google.com/mail/&ogbl"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Gmail
+      </a>
+      <a
+        className="App-link"
+        href="https://www.google.fr/imghp?hl=fr&ogbl"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Images
+      </a>
+    </header>
+  );
+}
+
+export default Header;
