dd2a069dc1bf869fc8805ecfddfce9f0c9667f8a
Pascal Vaccaro <pascal.vaccaro@gmail.com>
2023-02-10 13:32:22 +0100

    isole le composant Search

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
-import loupe from './loupe.svg';
-import micro from './micro.svg';
-import camera from './camera.js';
 import Header from './components/Header';
+import Search from './components/Search';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-
-      <main className="App-main">
-        <h1>
-          <span className="blue">G</span>
-          <span className="red">o</span>
-          <span className="yellow">o</span>
-          <span className="blue">g</span>
-          <span className="green">l</span>
-          <span className="red">e</span>
-        </h1>
-        <div className="search">
-          <span className="icon">
-            <img src={loupe} alt="Loupe" />
-          </span>
-          <input type="text" />
-          <span className="icon">
-            <img src={micro} alt="Micro" />
-          </span>
-          <span className="icon">
-            <img src={camera} width="20" height="20" alt="Camera" />
-          </span>
-        </div>
-        <section className="App-actions">
-          <button>Recherche Google</button>
-          <button>J'ai de la chance</button>
-        </section>
-      </main>
+      <Search />     
 
       <footer className="App-footer">
         France
---
+++ src/components/Search.js
@@ -0,0 +1,36 @@
+import loupe from '../loupe.svg';
+import micro from '../micro.svg';
+import camera from '../camera.js';
+
+function Search() {
+  return (
+    <main className="App-main">
+      <h1>
+        <span className="blue">G</span>
+        <span className="red">o</span>
+        <span className="yellow">o</span>
+        <span className="blue">g</span>
+        <span className="green">l</span>
+        <span className="red">e</span>
+      </h1>
+      <div className="search">
+        <span className="icon">
+          <img src={loupe} alt="Loupe" />
+        </span>
+        <input type="text" />
+        <span className="icon">
+          <img src={micro} alt="Micro" />
+        </span>
+        <span className="icon">
+          <img src={camera} width="20" height="20" alt="Camera" />
+        </span>
+      </div>
+      <section className="App-actions">
+        <button>Recherche Google</button>
+        <button>J'ai de la chance</button>
+      </section>
+    </main>
+  );
+}
+
+export default Search;
