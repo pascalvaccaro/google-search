d2a95746ffe2d385e050b78c4f1280b9b56482ae
Pascal Vaccaro <pascal.vaccaro@gmail.com>
2023-02-10 12:42:45 +0100

    prepare le css et le JSX pour fabriquer la page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +13,10 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <main className="App-main"></main>
+
+      <footer className="App-footer"></footer>
     </div>
   );
 }
